e0710fda6ab27f4b9558599bf3861beaf8ab0a2c
Xelo <williamchang435@gmail.com>
2019-07-20 20:37:24 +1000

    shader add

--- data/dither.glsl
+++ data/dither.glsl
@@ -19,26 +19,30 @@ uniform float [16] bayer;
 
 void main() {
   vec4 texColor = texture2D(texture, vertTexCoord.st).rgba;
-  vec2 g= mod(vertTexCoord.st*res,vec2(4.0));
-  int fg = int(mod(int(g.x)+int(g.y)*4+int(ditheroffset),16));
-  texColor.r=texColor.r*0.5+0.5;
-  float or = texColor.r;
-  float lor = float(int(or*2.0));
-  int res = 0;
-  if(2.0*mod(texColor.r,0.5)<=bayer[fg]/256.0){
-	res = int(round(lor));	
-  }else{
-	res = int(round(lor))+1;
+  if(texColor.a>0.1){
+	  vec2 g= mod(vertTexCoord.st*res,vec2(4.0));
+	  int fg = int(mod(int(g.x)+int(g.y)*4+int(ditheroffset*10.0*max( 1.0-(texColor.a+0.5),0.0)),16));
+	  texColor.r=texColor.r*0.5+0.5;
+	  float or = texColor.r;
+	  float lor = float(int(or*2.0));
+	  int res = 0;
+	  if(2.0*mod(texColor.r,0.5)<=bayer[fg]/256.0){
+		res = int(round(lor));	
+	  }else{
+		res = int(round(lor))+1;
+	  }
+	  if(res==0){
+		texColor.rgb = colordark;
+	  }
+	  if(res==1){
+		texColor.rgb = colormid;
+	  }
+	  if(res==2){
+		texColor.rgb = color;
+	  }
+	  texColor.a=1.0;
+	  
+	  
   }
-  if(res==0){
-	texColor.rgb = colordark;
-  }
-  if(res==1){
-	texColor.rgb = colormid;
-  }
-  if(res==2){
-	texColor.rgb = color;
-  }
-  
   gl_FragColor = texColor;
 }
